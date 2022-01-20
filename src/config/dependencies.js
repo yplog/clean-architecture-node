9fb37b56c03b6f225375a690fe044c2497fea203
yplog <yalinpala@protonmail.com>
2022-01-20 11:03:07 +0300

    mongodb users repository

--- src/config/dependencies.js
+++ src/config/dependencies.js
@@ -1,5 +1,6 @@
 const useCases = require('../useCases');
 const repositories = require('../frameworks/repositories/inMemory');
+//const repositories = require('../frameworks/repositories/mongo');
 
 module.exports = {
   useCases,
